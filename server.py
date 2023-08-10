7dcd55341194f902cac9169df0f7f3ac728b6688
Morgan Zero <57449785+morganzero@users.noreply.github.com>
2023-08-10 19:51:42 +0200

    Create server.py

---
+++ server.py
@@ -0,0 +1,31 @@
+import subprocess
+import os
+from flask import Flask, send_file, jsonify
+
+app = Flask(__name__)
+
+def fetch_logs():
+    try:
+        result = subprocess.check_output(['/app/auto-git.sh'])
+        logs = result.decode('utf-8')
+    except subprocess.CalledProcessError as e:
+        logs = f"Error running auto-git.sh: {e}"
+    
+    print(logs)  # Log the result to see if logs are being fetched correctly.
+    return logs
+
+@app.route('/')
+def index():
+    return send_file('index.html')
+
+@app.route('/logs')
+def get_logs():
+    logs = fetch_logs()
+    return logs
+
+@app.route('/background_image_url')
+def background_image_url():
+    return jsonify(url=os.environ.get("BACKGROUND_IMAGE_URL", ""))
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=8080)
