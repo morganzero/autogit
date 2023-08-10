1b2d74e5f6bf9a972c3da17cd753388a65dfec88
Morgan Zero <57449785+morganzero@users.noreply.github.com>
2023-08-10 20:42:52 +0200

    Update auto-git.sh

--- auto-git.sh
+++ auto-git.sh
@@ -1,42 +1,48 @@
 #!/bin/bash
 
-APP_NAMES=$(printenv | grep GITPATH_ | cut -d'_' -f2 | sort -u)
-
-auto_versioning() {
-    if [ "$AUTO_VERSIONING" = "true" ]; then
-        python3 /app/versioning.py "$MYREPO" "$MYFILE"
-        echo "Auto versioning applied to: $MYREPO"
-        git -C "$MYREPO" add "$MYFILE"
-        git -C "$MYREPO" commit -m "Auto-versioning applied"
-        git -C "$MYREPO" push
-    fi
-}
+MYREPO="$GITPATH"
 
 git_check() {
-    LOCAL=$(git -C "$MYREPO" rev-parse @)
-    REMOTE=$(git -C "$MYREPO" rev-parse @{u})
-    BASE=$(git -C "$MYREPO" merge-base @ @{u})
+  LOCAL=$(git -C "$MYREPO" rev-parse @)
+  REMOTE=$(git -C "$MYREPO" rev-parse @{u})
+  BASE=$(git -C "$MYREPO" merge-base @ @{u})
 
-    git -C "$MYREPO" remote update
+  git -C "$MYREPO" remote update
 
-    if [ "$LOCAL" = "$REMOTE" ]; then
-        echo "Up-to-date"
-    elif [ "$LOCAL" = "$BASE" ]; then
-        echo "Need to pull"
-        git -C "$MYREPO" pull
-        auto_versioning
-    fi
+  if [ "$LOCAL" = "$REMOTE" ]; then
+    echo "Up-to-date"
+  elif [ "$LOCAL" = "$BASE" ]; then
+    echo "Need to pull"
+    git -C "$MYREPO" pull
+  #elif [ "$REMOTE" = "$BASE" ]; then
+  #  echo "Need to push"
+  #  # Pushing the changes
+  #  git -C "$MYREPO" push origin $(git -C "$MYREPO" rev-parse --abbrev-ref HEAD)
+  #else
+  #  echo "Diverged"
+  #  # Pulling remote changes, merging them, and then pushing the merged changes
+  #  git -C "$MYREPO" pull --rebase && git -C "$MYREPO" push
+  fi
 }
 
 last_commit_msg() {
-    echo
-    echo "Last commit-message:"
-    git -C "$MYREPO" log -1 --pretty=format:"%B"
+  echo
+  echo "Last commit message:"
+  git -C "$MYREPO" log -1 --pretty=format:"%B"
+}
+
+auto_versioning() {
+  if [ "$AUTO_VERSIONING" = "true" ]; then
+    python3 /app/versioning.py
+    source "${MYREPO}"
+    echo "Auto version to: ${MYREPO}"
+  fi
 }
 
 add_date() {
-    awk '{ print strftime("%Y-%m-%d %T"), $0 }'
+  awk '{ print strftime("%Y-%m-%d %T"), $0 }'
 }
-    
+
 git_check 2>&1 | add_date
+auto_versioning 2>&1 | add_date
 last_commit_msg
