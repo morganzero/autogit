546d502a11b4641e1fcaa604b5d32e2d5ef81d23
Morgan Zero <57449785+morganzero@users.noreply.github.com>
2023-08-11 13:09:35 +0200

    autogit

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,5 @@
 FROM alpine:latest
+
 LABEL maintainer="morganzero@sushibox.dev" \
       description="Automatic pulls and versioning" \
       name="AutoGit"
@@ -8,10 +9,13 @@ RUN apk --no-cache add git bash python3 py3-pip && \
     rm -rf /var/cache/apk/*
 
 WORKDIR /app
+
 COPY assets auto-git.sh versioning.py index.html server.py /app/
+
 RUN chmod +x /app/auto-git.sh
 
 EXPOSE 8080
+
 ENV GITPATH=""
 ENV GITFILEPATH=""
 ENV EXEC_INTERVAL=60
--- server.py
+++ server.py
@@ -1,7 +1,18 @@
 import subprocess
+import logging
 from flask import Flask, send_from_directory, send_file
+from flask_talisman import Talisman
 
 app = Flask(__name__, static_folder='assets')
+csp = {
+    'default-src': "'self'",
+    'style-src': "'self' 'unsafe-inline'",
+    'img-src': "'self' data:",
+}
+Talisman(app, content_security_policy=csp)
+
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
 
 def fetch_logs():
     try:
@@ -17,8 +28,12 @@ def index():
 
 @app.route('/logs')
 def get_logs():
-    logs = fetch_logs()
-    return logs
+    try:
+        logs = fetch_logs()
+        return logs
+    except Exception as e:
+        logger.error(f"Error fetching logs: {e}")
+        return "Error fetching logs.", 500
 
 @app.route('/assets/favicon.ico')
 def favicon():
@@ -29,4 +44,4 @@ def serve_static(filename):
     return send_from_directory(app.static_folder, filename)
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8080)
+    app.run(host='0.0.0.0', port=8080, debug=False)
