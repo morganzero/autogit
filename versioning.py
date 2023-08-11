4ecb29e46f6f40716d154acab9cc9ba4fca40fbf
Morgan Zero <57449785+morganzero@users.noreply.github.com>
2023-08-11 14:32:56 +0200

    versiniong

--- versioning.py
+++ versioning.py
@@ -1,79 +1,57 @@
 import re
 import subprocess
 import os
-import sys
-import fcntl
 
-MYREPO = sys.argv[1]
-MYFILE = sys.argv[2]
+MYREPO = os.environ.get("GITPATH")
+MYFILE = os.environ["GITFILEPATH"]
+
 
 if not os.path.exists(MYFILE):
     raise FileNotFoundError(f"Version file not found at path: {MYFILE}")
 
-def get_bump_type_from_commit():
-    commit_message = subprocess.check_output(["git", "-C", MYREPO, "log", "-1", "--pretty=%B"]).decode("utf-8").lower()
-    
-    if "major" in commit_message:
-        return "major"
-    elif "minor" in commit_message:
-        return "minor"
-    else:  # Default to patch if no specific keyword is found.
-        return "patch"
-
-def get_version_for_branch(branch):
+def get_current_versions():
     with open(MYFILE, 'r') as version_file:
         content = version_file.read()
 
-    branch_match = re.search(f"{branch}='(v\d+\.\d+\.\d+)'", content)
-    if branch_match:
-        branch_version = branch_match.group(1)
-        return branch_version
-    else:
-        raise ValueError(f"Couldn't find version pattern for branch: {branch} in the file")
+    latest_match = re.search(r"latest='(v\d+\.\d+\.\d+)'", content)
+    develop_match = re.search(r"develop='(v\d+\.\d+\.\d+)'", content)
 
-def increment_version(version, bump_type="patch"):
-    major, minor, patch = map(int, version[1:].split('.'))
-    
-    if bump_type == "major":
-        major += 1
-        minor = 0
-        patch = 0
-    elif bump_type == "minor":
-        minor += 1
-        patch = 0
-    elif bump_type == "patch":
-        patch += 1
+    if latest_match and develop_match:
+        latest_version = latest_match.group(1)
+        develop_version = develop_match.group(1)
+        return latest_version, develop_version
     else:
-        raise ValueError(f"Unknown bump type: {bump_type}")
-    
+        raise ValueError("Couldn't find version patterns in the file")
+
+def increment_version(version):
+    major, minor, patch = map(int, version[1:].split('.'))
+    patch += 1
     return f"v{major}.{minor}.{patch}"
 
 def update_version_file(branch):
-    bump_type = get_bump_type_from_commit()
-    branch_version = get_version_for_branch(branch)
-    new_branch_version = increment_version(branch_version, bump_type)
-    update_version_file_for_branch(new_branch_version, branch)
+    latest_version, develop_version = get_current_versions()
+    
+    if branch == "latest":
+        new_latest_version = increment_version(latest_version)
+        update_version_file_in_branch(new_latest_version, "latest")
+    elif branch == "develop":
+        new_develop_version = increment_version(develop_version)
+        update_version_file_in_branch(new_develop_version, "develop")
+    else:
+        print("Unknown branch:", branch)
 
-def update_version_file_for_branch(new_version, branch):
-    with open(MYFILE, 'r+') as version_file:
-        # Lock the file
-        fcntl.flock(version_file, fcntl.LOCK_EX)
-
+def update_version_file_in_branch(new_version, branch):
+    with open(MYFILE, 'r') as version_file:
         content = version_file.read()
-        content = re.sub(f"{branch}='v\d+\.\d+\.\d+'", f"{branch}='{new_version}'", content)
+    
+    content = re.sub(f"{branch}='v\d+\.\d+\.\d+'", f"{branch}='{new_version}'", content)
 
-        # Move the pointer to the beginning of the file
-        version_file.seek(0)
+    with open(MYFILE, 'w') as version_file:
         version_file.write(content)
-
-        # Ensure the file is truncated to the size of our data
-        version_file.truncate()
-
-        # Unlock the file
-        fcntl.flock(version_file, fcntl.LOCK_UN)
 
 def main():
     current_branch = subprocess.check_output(["git", "-C", MYREPO, "rev-parse", "--abbrev-ref", "HEAD"]).decode("utf-8").strip()
+    
     update_version_file(current_branch)
 
 if __name__ == "__main__":
