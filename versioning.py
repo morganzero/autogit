b9578ad78ec1f082c0bc6c7442cefab7d5b40122
Morgan Zero <57449785+morganzero@users.noreply.github.com>
2023-08-11 15:11:29 +0200

    we will see

--- versioning.py
+++ versioning.py
@@ -14,11 +14,15 @@ def get_current_versions():
 
     latest_match = re.search(r"latest='(v\d+\.\d+\.\d+)'", content)
     develop_match = re.search(r"develop='(v\d+\.\d+\.\d+)'", content)
+    main_match = re.search(r"main='(v\d+\.\d+\.\d+)'", content)
+    beta_match = re.search(r"beta='(v\d+\.\d+\.\d+)'", content)
 
-    if latest_match and develop_match:
+    if latest_match and develop_match and main_match and beta_match:
         latest_version = latest_match.group(1)
         develop_version = develop_match.group(1)
-        return latest_version, develop_version
+        main_version = main_match.group(1)
+        beta_version = beta_match.group(1)
+        return latest_version, develop_version, main_version, beta_version
     else:
         raise ValueError("Couldn't find version patterns in the file")
 
@@ -37,15 +41,15 @@ def increment_version(version, level):
     
     return f"v{major}.{minor}.{patch}"
 
-def update_version_file(branch, level):
-    latest_version, develop_version = get_current_versions()
-    
+def update_version_file(branch, new_version):
     if branch == "latest":
-        new_latest_version = increment_version(latest_version, level)
-        update_version_file_in_branch(new_latest_version, "latest")
+        update_version_file_in_branch(new_version, "latest")
     elif branch == "develop":
-        new_develop_version = increment_version(develop_version, level)
-        update_version_file_in_branch(new_develop_version, "develop")
+        update_version_file_in_branch(new_version, "develop")
+    elif branch == "main":
+        update_version_file_in_branch(new_version, "main")
+    elif branch == "beta":
+        update_version_file_in_branch(new_version, "beta")
     else:
         print("Unknown branch:", branch)
 
@@ -58,17 +62,30 @@ def update_version_file_in_branch(new_version, branch):
     with open(MYFILE, 'w') as version_file:
         version_file.write(content)
 
+def rollback_version(new_version):
+    update_version_file_in_branch(new_version, "latest")
+    update_version_file_in_branch(new_version, "develop")
+    update_version_file_in_branch(new_version, "main")
+    update_version_file_in_branch(new_version, "beta")
+
 def main():
     current_branch = subprocess.check_output(["git", "-C", MYREPO, "rev-parse", "--abbrev-ref", "HEAD"]).decode("utf-8").strip()
 
     commit_message = subprocess.check_output(["git", "-C", MYREPO, "log", "-1", "--pretty=%B"]).decode("utf-8").strip()
-    level = "patch"
-    if "minor" in commit_message:
-        level = "minor"
-    elif "major" in commit_message:
-        level = "major"
     
-    update_version_file(current_branch, level)
+    if "rollback" in commit_message:
+        match = re.search(r"rollback (\d+\.\d+\.\d+)", commit_message)
+        if match:
+            rollback_version(match.group(1))
+    else:
+        level = "patch"
+        if "minor" in commit_message:
+            level = "minor"
+        elif "major" in commit_message:
+            level = "major"
+        
+        new_version = increment_version(get_current_versions()[0], level)
+        update_version_file(current_branch, new_version)
 
 if __name__ == "__main__":
     main()
